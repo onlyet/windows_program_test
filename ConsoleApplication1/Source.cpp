f90a257a2059b1c8e82e94dc84257301586210fe
onlyet <1369212327@qq.com>
2019-05-31 18:01:51 +0800

    '1'

---
+++ ConsoleApplication1/Source.cpp
@@ -0,0 +1,48 @@
+// crt_vsnwprintf.c
+// compile by using: cl /W3 crt_vsnwprintf.c
+
+// To turn off error C4996, define this symbol:
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <wtypes.h>
+
+#define BUFFCOUNT (10)
+
+void FormatOutput(LPCWSTR formatstring, ...)
+{
+    int nSize = 0;
+    wchar_t buff[BUFFCOUNT];
+    memset(buff, 0, sizeof(buff));
+    va_list args;
+    va_start(args, formatstring);
+    // Note: _vsnwprintf is deprecated; consider vsnwprintf_s instead
+    nSize = _vsnwprintf(buff, BUFFCOUNT - 1, formatstring, args); // C4996
+    wprintf(L"nSize: %d, buff: %ls\n", nSize, buff);
+    va_end(args);
+}
+
+int main() {
+    FormatOutput(L"%ls %ls", L"Hi", L"there");
+    FormatOutput(L"%ls %ls", L"Hi", L"there!");
+    FormatOutput(L"%ls %ls", L"Hi", L"there!!");
+    system("pause");
+}
+
+//void FormatOutput(char* formatstring, ...)
+//{
+//    int nSize = 0;
+//    char buff[BUFFCOUNT];
+//    memset(buff, 0, sizeof(buff));
+//    va_list args;
+//    va_start(args, formatstring);
+//    nSize = _vsnprintf(buff, sizeof(buff), formatstring, args);
+//    printf("nSize: %d, buff: %s\n", nSize, buff);
+//    va_end(args);
+//}
+//
+//int main() {
+//    FormatOutput("%s %s", "Hi", "there");   //  8 chars + null
+//    FormatOutput("%s %s", "Hi", "there!");  //  9 chars + null
+//    FormatOutput("%s %s", "Hi", "there!!1234"); // 10 chars + null
+//}
